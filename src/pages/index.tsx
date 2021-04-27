30879e331fbf942ee34d163e3d118c8b31f1663f
Renan Souza <renansouzaa81@gmail.com>
2021-04-27 12:32:46 -0300

    Aula 3 28:41

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,19 @@
-import { useEffect } from "react"
+import { GetServerSideProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+}
+
+type HomeProps = {
+  episodes: Episodes[];
+}
 
 export default function Home(props) {
   return (
@@ -9,13 +24,32 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes') 
-  const data = await response.json()
+export const getStaticProps: GetServerSideProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  }) 
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt:  format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }
