0194d15f387f50439e55fe8420256a5c8684c003
Renan Souza <renansouzaa81@gmail.com>
2021-05-05 09:48:53 -0300

    Final Aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -130,7 +129,6 @@ export const getStaticProps: GetServerSideProps = async () => {
       publishedAt:  format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
     };
   })
